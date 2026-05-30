4ef4dbb282368bc55addc70bc127bd4c9e465918
Nathaniel <nbauer12@terpmail.umd.edu>
2026-05-30 09:45:25 -0400

    setup + cmake

---
+++ ServerThread.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "ServerThread.h"
+
+std::string ServerThread::printtest()
+{
+    return "Hello World";
+}
---
+++ ServerThread.h
@@ -0,0 +1,7 @@
+#include <string>
+
+class ServerThread
+{
+public:
+    std::string printtest();
+};
